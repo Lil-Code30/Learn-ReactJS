17fa0526e4de538db4f86bf6a773538fc1b73b0f
Ismael Loko <lokoismael30@gmail.com>
2025-05-26 00:26:12 -0400

    challenge 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles.css';
-import App from './App.jsx';
+import { createRoot } from "react-dom/client"
+import './styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const root = createRoot(document.getElementById("root"))
+
+/**
+ * Challenge (part 1):
+ * Create a custom "Page" component
+ * 
+ * It should return an ordered list with the reasons why you're
+ * excited to be learning React :)
+ * 
+ * Render the Page component.
+ */
+
+function Page(){
+  return (
+    <ol>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ol>
+  )
+}
+
+
+root.render(
+    <main>
+        <img src="/react-logo.png"  alt="React logo" />
+        <h1>Fun facts about React!</h1>
+        <Page/>
+    </main>
 )
